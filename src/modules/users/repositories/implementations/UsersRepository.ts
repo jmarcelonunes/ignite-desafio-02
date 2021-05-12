3e8029d10545ec4f2477af52b0acc1f596da295f
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2021-05-12 14:12:04 -0300

    Finaliza desafio 02

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -2,41 +2,51 @@ import { User } from "../../model/User";
 import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
 
 class UsersRepository implements IUsersRepository {
-  private users: User[];
+    private users: User[];
 
-  private static INSTANCE: UsersRepository;
+    private static INSTANCE: UsersRepository;
 
-  private constructor() {
-    this.users = [];
-  }
-
-  public static getInstance(): UsersRepository {
-    if (!UsersRepository.INSTANCE) {
-      UsersRepository.INSTANCE = new UsersRepository();
+    private constructor() {
+        this.users = [];
     }
 
-    return UsersRepository.INSTANCE;
-  }
+    public static getInstance(): UsersRepository {
+        if (!UsersRepository.INSTANCE) {
+            UsersRepository.INSTANCE = new UsersRepository();
+        }
 
-  create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
-  }
+        return UsersRepository.INSTANCE;
+    }
 
-  findById(id: string): User | undefined {
-    // Complete aqui
-  }
+    create({ name, email }: ICreateUserDTO): User {
+        const user = new User();
+        Object.assign(user, {
+            name,
+            email,
+            admin: false,
+            created_at: new Date(),
+            updated_at: new Date(),
+        });
+        this.users.push(user);
+        return user;
+    }
 
-  findByEmail(email: string): User | undefined {
-    // Complete aqui
-  }
+    findById(id: string): User | undefined {
+        return this.users.find((user) => user.id === id);
+    }
 
-  turnAdmin(receivedUser: User): User {
-    // Complete aqui
-  }
+    findByEmail(email: string): User | undefined {
+        return this.users.find((user) => user.email === email);
+    }
 
-  list(): User[] {
-    // Complete aqui
-  }
+    turnAdmin(receivedUser: User): User {
+        receivedUser.turnAdminOn();
+        return receivedUser;
+    }
+
+    list(): User[] {
+        return this.users;
+    }
 }
 
 export { UsersRepository };
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -2,15 +2,19 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class TurnUserAdminUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User {
+        const userExists = this.usersRepository.findById(user_id);
+        if (!userExists) {
+            throw new Error("user not found");
+        }
+        return this.usersRepository.turnAdmin(userExists);
+    }
 }
 
 export { TurnUserAdminUseCase };
